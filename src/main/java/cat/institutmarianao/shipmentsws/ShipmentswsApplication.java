be8f3e5b079e5f247c4ef9056002fce0a37a9d0a
marcrebullida2004 <marcrebullidalozano@gmail.com>
2024-01-24 21:19:28 +0100

    i18n (esta a medias, otra vez)

--- src/main/java/cat/institutmarianao/shipmentsws/ShipmentswsApplication.java
+++ src/main/java/cat/institutmarianao/shipmentsws/ShipmentswsApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @PropertySource("classpath:openapi.properties")
@@ -22,6 +27,9 @@ public class ShipmentswsApplication {
 	}
 
 	public static void main(String[] args) {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("resources/i18n/messages.properties");
+		messageSource.setDefaultEncoding("UTF-8");
 		SpringApplication.run(ShipmentswsApplication.class, args);
 	}
 

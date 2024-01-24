85b6af3e17dcf4407b4c8e4fa26b2b10f70a7ad8
Victoria Casasampere Fernandez <vickyf5124@gmail.com>
2024-01-24 11:36:22 +0100

    Filter findAll 	modified:   src/main/java/cat/institutmarianao/shipmentsws/services/impl/ShipmentServiceImpl.java

--- src/main/java/cat/institutmarianao/shipmentsws/services/impl/ShipmentServiceImpl.java
+++ src/main/java/cat/institutmarianao/shipmentsws/services/impl/ShipmentServiceImpl.java
@@ -2,6 +2,8 @@ package cat.institutmarianao.shipmentsws.services.impl;
 
 import cat.institutmarianao.shipmentsws.exception.NotFoundException;
 import cat.institutmarianao.shipmentsws.model.Action;
+import cat.institutmarianao.shipmentsws.model.Assignment;
+import cat.institutmarianao.shipmentsws.model.Courier;
 import cat.institutmarianao.shipmentsws.model.Shipment;
 import cat.institutmarianao.shipmentsws.repositories.ShipmentRepository;
 import cat.institutmarianao.shipmentsws.services.ShipmentService;
@@ -16,6 +18,8 @@ import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 @Validated
 @Service
@@ -25,9 +29,54 @@ public class ShipmentServiceImpl implements ShipmentService {
 
     @Override
     @Transactional(readOnly = true)
-    public List<Shipment> findAll(Shipment.Status status, String receivedBy, String courierAssigned, Shipment.Category category, Date from, Date to) {
-        // TODO specification
-        return shipmentRepository.findAll();
+    public List<Shipment> findAll(Shipment.Status status, String receivedBy, String courierAssigned, Shipment.Category category, Date from, Date to) { // TODO specification
+        return shipmentRepository
+                .findAll()
+                .stream()
+                .filter(i -> {
+                    if (status != null) {
+                        return i.getStatus() == status;
+                    }
+                    return true;
+                })
+                .filter(i -> {
+                    if (receivedBy != null) {
+                        return Objects.equals(i.getRecipient().getName().toLowerCase(Locale.ROOT), receivedBy.toLowerCase());
+                    }
+                    return true;
+                })
+                .filter(i -> {
+                    if (courierAssigned != null) {
+                        Assignment x = (Assignment) i.getTracking().stream().filter(j -> j instanceof Assignment).toList().get(0);
+                        return Objects.equals(x.getCourier().getFullName().toLowerCase(Locale.ROOT), courierAssigned.toLowerCase());
+                    }
+                    return true;
+                })
+                .filter(i -> {
+                    if (category != null) {
+                        return i.getCategory() == category;
+                    }
+                    return true;
+                })
+                .filter(i -> {
+                    if (from != null) {
+                        if (i.getTracking().get(0) != null) {
+                            return i.getTracking().get(0).getDate().after(from);
+                        }
+                        return true;
+                    }
+                    return true;
+                })
+                .filter(i -> {
+                    if (to != null) {
+                        if (i.getTracking().get(i.getTracking().size()) != null) {
+                            return i.getTracking().get(0).getDate().before(to);
+                        }
+                        return true;
+                    }
+                    return true;
+                })
+                .toList();
     }
 
     @Override
